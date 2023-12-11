f7ee5435a019d235a607a26c303726eaa676dab5
rodrigomaruya <109206973+rodrigomaruya@users.noreply.github.com>
2023-12-11 20:34:14 +0900

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ btn_comecar.addEventListener("click",()=>{
     gameCampo.classList.remove("ocultar")
     comecar=setInterval(moveCirculo,350)
     setTimeout(parar,6000)
-    setTimeout(som,5800)
+    setTimeout(som,5700)
     setTimeout(piscar,3000)
     setTimeout(paraPiscar,10000)
     
